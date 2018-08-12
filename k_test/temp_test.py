6f1ef83e29045307a62ec0e90dbebf1e5a566d2d
kryfo <superdkk@gmail.com>
2018-08-12 16:11:20 +0800

    home test git

--- k_test/temp_test.py
+++ k_test/temp_test.py
@@ -3,6 +3,3 @@ import os
 ppath = '\\\\10.80.100.201\\d\\outputdata5\\1807000\\1807081\\19218898_mns024_0380_cg_extension_master_01ddddtdddd'
 aa=os.path.normpath(ppath)
 print (aa)
-
-
-print ('hello git')
